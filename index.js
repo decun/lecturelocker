05edda9b6d51d041126470baa88d39f230f0153d
decun <diazfarid10@gmail.com>
2023-04-19 19:42:17 -0400

    aaa

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+const { google } = require('googleapis');
+require('dotenv').config();
+
+const apiKey = process.env.API_KEY;
+const youtube = google.youtube({
+  version: 'v3',
+  auth: apiKey
+});
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/api/hola', ctx => {
-  ctx.body = 'Hola, mundo!';
+
+
+router.get('/embed/:idVideo', async (ctx) => {
+  const idVideo = ctx.params.idVideo;
+  const response = await youtube.videos.list({
+    id: idVideo,
+    part: 'player'
+  });
+  const embed = response.data.items[0].player.embedHtml;
+  ctx.body = embed;
 });
 
-router.post('/api/saludo', ctx => {
-  const { nombre } = ctx.request.body;
-  ctx.body = `Hola, ${nombre}!`;
-});
-
-app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen(3000, () => {
   console.log('Servidor iniciado en el puerto 3000');
+});
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  // Leer el archivo index.html
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      // Si hay un error al leer el archivo, mostrar un mensaje de error
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.write('<h1>Error al leer el archivo index.html</h1>');
+      return res.end();
+    }
+
+    // Si no hay errores, mostrar el archivo index.html en el navegador
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
 });
